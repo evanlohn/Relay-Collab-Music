80c9b82f4b5b22f6860ca2d9f866f088549c5979
evanlohn <evan.lohn@berkeley.edu>
2025-03-02 12:29:50 -0800

    migration 1

---
+++ migrations/1740855020275_basic-user-session.js
@@ -0,0 +1,48 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.createTable('sessions', {
+        id: 'id',
+        body: { type: 'jsonb', notNull: true },
+        createdAt: {
+          type: 'timestamp',
+          notNull: true,
+          default: pgm.func('current_timestamp'),
+        },
+    });
+    pgm.createTable('users', {
+      id: 'id',
+      name: { type: 'varchar(1000)', notNull: true },
+      clef: { type: 'integer', notNull: true},
+      isHost: { type: 'boolean', notNull: true},
+      sessionId: {
+        type: 'integer',
+        notNull: true,
+        references: '"sessions"',
+        onDelete: 'cascade',
+      },
+      createdAt: {
+        type: 'timestamp',
+        notNull: true,
+        default: pgm.func('current_timestamp'),
+      },
+    });
+    pgm.createIndex('users', 'sessionId');
+  };
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropTable('users');
+    pgm.dropTable('sessions');
+};
