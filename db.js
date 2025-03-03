c5d278b92b58a51b6bafe5e2b0e89ec925274c81
Rebecca Abraham <33535365+beccyabraham@users.noreply.github.com>
2025-03-02 22:26:56 -0500

    add ssl param to db connection

--- db.js
+++ db.js
@@ -3,6 +3,9 @@ const { Pool } = require('pg');
 // Create a pool for managing PostgreSQL connections
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL, // Ensure this variable is set
+    ssl: {
+        rejectUnauthorized: false
+    },
     max: 20,  // Set the maximum number of clients in the pool
 });
 
